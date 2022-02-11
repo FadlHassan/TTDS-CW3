a3b7891d6ca215f44ef17b4c6414bdbd638d3565
Eshaan Manglik <49763476+eshaan13@users.noreply.github.com>
2022-02-11 18:12:50 +0000

    File with final db schema

--- data_collection/db.py
+++ data_collection/db.py
@@ -1,57 +1,100 @@
 import sqlite3
 from sqlite3 import Error
 
+import lyricsgenius
+import pandas as pd
+import csv
+import sys
+
 
 def main():
-    database = r"pythonsqlite.db"
+    database3 = r"lyricsDB.db"
 
     # Add data to the tables
     
     # create a database connection
-    conn = create_connection(database)
-    with conn:
-        
-        # # create a new artist
-        # artist_1 = ("The Weeknd");
-        # artist_id = create_artist(conn, artist_1)
+    conn = create_connection(database3)
 
-        # # create new tracks
-        # track_1 = ('Save Your Tears', '2015-01-01', 'pop', "Save your tears", 'picture_save_your_tears', artist_id)
-        # track_2 = ('Blinding Lights', '2015-01-01', 'pop', "I've been trynna call", 'picture_blinding_lights', artist_id)
-        # track_3 = ('Hills', '2015-01-01', 'pop', "I only call you when its half past 5 the only time I ever call you mine.", 'picture_hills', artist_id)
+    with conn:
+
+
+        # # Table to save all songs
+        # All_Songs_Table = pd.DataFrame(columns=['artist','title','lyrics'])
+        # LyricsGenius = connectToGenius()
+        # artists = getArtists()
+        # artist_cat = [(0, 225499), (225499, 450998), (450998, 676497), (676497, 901996), (901996, 1127495), (1127495, 1352994)]
+        # getLyrics(LyricsGenius, artists, artist_cat,All_Songs_Table)
+        
+
+        # # Create new tracks
+        # track_1 = ('Passion Fruit', "Passion fruit lyrics", "Drake")
+        # track_2 = ('Toosie Slide', "Toosie slide lyrics", "Drake")
+        # track_3 = ('Free Smoke', "Free smoke lyrics", "Drake")
+        # track_4 = ('Astroworld', "Astroworld lyrics", "Travis Scott")
+        # track_5 = ('London', "London lyrics", "Travis Scott")
 
         # # METHODS TO CREATE Artist & Track
         # create_track(conn, track_1)
         # create_track(conn, track_2)
+        # create_track(conn, track_3)
+        # create_track(conn, track_4)
+        # create_track(conn, track_5)
+
+        # # METHODS TO GET all the Artists & Tracks
+
+        # print("--- All Tracks")
+        # select_all_tracks(conn)
+
+        # # METHOD to get all Tracks from a particular Artist
+        # print("--- Tracks from The Weeknd")
+        # select_tracks_by_artist(conn, "The Weeknd")
+
+        # # METHOD to get all Tracks from a particular Artist
+        # print("--- Tracks from Post Malone")
+        # select_tracks_by_artist(conn, "Post Malone")
 
 
-        # # create a new artist
-        # artist_2 = ("Post Malone");
-        # artist_id = create_artist(conn, artist_2)
 
-        # # create new tracks
-        # track_1 = ('Circle', '2015-01-01', 'pop', "We couldn't turn around 'Til we were upside down I'll be the bad guy now But no, I ain't too proud", 'picture_circles', artist_id)
-        # track_2 = ('Flex', '2015-01-01', 'pop', "Lighting stog after stog, choke on the smoke. They tell me to quit, don't listen what. I'm told Help me forget that this world is so cold", 'picture_flex', artist_id)
+def connectToGenius():
+    client_access_token = 'v1RaGoN7_VBkkydj6ZpiK9wlaF-CbW-DIf_wgENhQETztCWFLpRyaHfs8THDSMfC'
+    LyricsGenius = lyricsgenius.Genius(client_access_token)
+    LyricsGenius.timeout = 5
+    LyricsGenius.sleep_time = 0.2
+    LyricsGenius.retries = 3
 
-        # METHODS TO CREATE Artist & Track
-        # create_track(conn, track_1)
-        # create_track(conn, track_2)
+    return LyricsGenius
 
-        # METHODS TO GET all the Artists & Tracks
-        print("--- All Artists")
-        select_all_artists(conn)
+def getArtists():
+    col_list = ['artist_mb']
+    df = pd.read_csv('artists.csv', usecols=col_list)
+    artists = df.values.tolist() 
+    artists = [artist[0] for artist in artists]
+    artists = list(dict.fromkeys(artists))
+    return artists
 
-        print("--- All Tracks")
-        select_all_tracks(conn)
+def getLyrics(LyricsGenius, artists, artist_cat,All_Songs_Table):
+    Collected_Lyrics = 0 
+    count = 0
+    cat = artist_cat[int(sys.argv[1])]
+    for i in range(cat[0],cat[1]):
+        Artist_Name = artists[i]
 
-        # METHOD to get all Tracks from a particular Artist
-        print("--- Tracks from The Weeknd")
-        select_tracks_by_artist(conn, "The Weeknd")
+        try:
+            artist = LyricsGenius.search_artist(Artist_Name, max_songs=100)
+            for song in artist.songs:
+                lyrics = song.lyrics   
+                lyrics = lyrics.replace(song.title + ' Lyrics', '')
 
-        # METHOD to get all Tracks from a particular Artist
-        print("--- Tracks from Post Malone")
-        select_tracks_by_artist(conn, "Post Malone")
 
+                # add artist & lyrics to DB using the "song" instance
+
+                All_Songs_Table = All_Songs_Table.append({'artist': song.artist, 'title':song.title,'lyrics':lyrics}, ignore_index=True) 
+                headerList = ['Artist', 'Song', 'Lyrics']
+                
+                lines_written = len(All_Songs_Table.index)
+                All_Songs_Table.iloc[lines_written-1:].to_csv('Songs_Table.csv', mode='a', header=False)
+        except:
+            continue
 
 
 
@@ -71,19 +114,6 @@ def create_connection(db_file):
 
     return conn
 
-def create_artist(conn, artist):
-    """
-    Create a new artist into the artists table
-    :param conn:
-    :param artist:
-    :return: artist id
-    """
-    sql = ''' INSERT INTO artists(name)
-              VALUES(?) '''
-    cur = conn.cursor()
-    cur.execute(sql, (artist,))
-    conn.commit()
-    return cur.lastrowid
 
 def create_track(conn, track):
     """
@@ -93,27 +123,12 @@ def create_track(conn, track):
     :return: track id
     """
 
-    sql = ''' INSERT INTO tracks(title,date,genre,lyrics,picture,artistId)
-              VALUES(?,?,?,?,?,?) '''
+    sql = ''' INSERT INTO tracks(title,lyrics,artist)
+              VALUES(?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, track)
     conn.commit()
     return cur.lastrowid
-
-
-def select_all_artists(conn):
-    """
-    Query all rows in the artists table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM artists")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
 
 
 def select_all_tracks(conn):
@@ -138,7 +153,7 @@ def select_tracks_by_artist(conn, artistName):
     :return:
     """
     cur = conn.cursor()
-    cur.execute("SELECT * FROM artists A JOIN tracks T ON A.id = T.artistId WHERE A.name =?", (artistName,))
+    cur.execute("SELECT * FROM tracks WHERE artist =?", (artistName,))
 
     rows = cur.fetchall()
 
